b9b2503e8f24eaa937babffc1282bb43c9af0238
youRock19 <yyyuras191@mail.ru>
2022-05-15 15:35:14 +0300

    version 1.0

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,13 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        
+        HelloWorld bean2 = 
+                (HelloWorld) applicationContext.getBean("helloworld");
+        Cat catBean1 = (Cat) applicationContext.getBean("cat");
+        Cat catBean2 = (Cat) applicationContext.getBean("cat");
+        
+        System.out.println("Бины Helloworld равны? " + bean.equals(bean2));
+        System.out.println("Бины Cat равны? " + catBean1.equals(catBean2));
     }
 }
